0a72d50810c94c3d726784eef2785018d743c997
KozlowskiKamil <kamil2k8@gmail.com>
2023-07-25 22:10:57 +0200

    initial commit

---
+++ src/test/java/com/jabil/downtime/DowntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabil.downtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowntimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
