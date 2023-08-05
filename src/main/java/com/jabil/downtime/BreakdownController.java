e9a660251eaea729be6b2420b3fe78c3bdc3e1a3
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-05 03:27:04 +0200

    add new filed edit firebase service

--- src/main/java/com/jabil/downtime/BreakdownController.java
+++ src/main/java/com/jabil/downtime/BreakdownController.java
@@ -13,9 +13,10 @@ import java.util.List;
 @RestController
 public class BreakdownController {
 
-    final String token1 = "dXccypV5QSuNnyqmHocZoA:APA91bGuVlBeFpjA9Mmnmg_ZBz_yNNqA1hye_26WbEiAbU9BYxiG0tOXGpDgjBfIgXmFBz4XLBBwoJX9jKG1C83NfFsGNisbfXOIQV6n6xGkvIZ62fAovZF0teJclr0jSMoq03uQ2sht";
+    final String token1 = "dcrBS40mTBOo8YlgydBkyq:APA91bGli-sGk8wlWy1uKZeZ2BocOcBaJa1XeCbuv5b2FLdC53o1HzyyM-qwcIo3WtkhTiCjsEdJZgbQvNTnLIQI5mbQksd8NLN3YA0584cslDKaYTU79Uc16hQEfZS7qGX5hJMjyI4u";
     final String token2 = "fpyPHgl-Tsez78NnFhmZ21:APA91bEjlpG-sTAJn_d_VqK2y9xKOktOE5q7WzwRBiHLOVCrt2Ai75uy-FS9S_LzTPPTRPgx0BUUT5D331Ta52Bm0hKA5M6u7eBjciTJyuBSpbE8gqsctMoRkw---3vZA9nMmcjLCW4L";
     final String token3 = "eokZ6Dw2TPaUQyLqyPjsnU:APA91bG7eaPvQE7a-mBY6Plf6pF8u1XG-ihBGBfErdwE2o6TfbIjV0pcj3rTfEw6Pbxmq-gwjTsz0PZ-UbXudnj4wgHIK_MkV0tE7r1Ije-p9WbgebRn-lH_z8ZJ8C0_3P10v5s8mnBd";
+    final String token4 = "dpmdkHn3RGqo_j7YVUNQXn:APA91bHBZRJW-LLto5tx-ShfdooJBpnWeDdR-m60zV5pTqLf2hyPbG7qVqLMGDVaCGMLT6OW-Cn0csFAQGbrAsPT8PIVEO6qxcsPzMGZ6_u3hbzATL_swha-irbTXARJCllipq0WNYY7";
 
 
     private static final Logger logger = LoggerFactory.getLogger(BreakdownController.class);
@@ -38,7 +39,8 @@ public class BreakdownController {
     public ResponseEntity<Breakdown> addBreakdown(@RequestBody Breakdown breakdown) {
         Breakdown savedBreakdown = breakdownService.saveBreakdown(breakdown);
         logger.info("Dodano nową awarię");
-        firebaseMessagingService.sendNotificationByToken(new NotificationMessage(token3, breakdown.getComputerName(), breakdown.getFailureName()));
+        firebaseMessagingService.sendNotificationByToken(new NotificationMessage(token1, breakdown.getComputerName(), breakdown.getFailureName()));
+        firebaseMessagingService.sendNotificationByToken(new NotificationMessage(token4, breakdown.getComputerName(), breakdown.getFailureName()));
         return new ResponseEntity<>(savedBreakdown, HttpStatus.CREATED);
     }
 
