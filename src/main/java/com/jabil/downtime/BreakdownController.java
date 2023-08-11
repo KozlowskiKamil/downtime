6bcd014f8a89d716713301dccb259e10ac208dba
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-11 20:05:01 +0200

    refactor

--- src/main/java/com/jabil/downtime/BreakdownController.java
+++ src/main/java/com/jabil/downtime/BreakdownController.java
@@ -21,7 +21,7 @@ public class BreakdownController {
     private static final Logger logger = LoggerFactory.getLogger(BreakdownController.class);
     final String token1 = "e3e_TLX_TxSK3ii63nqLjW:APA91bFSaFIEMzBTpRmu7AN0uOrfDwOauYA53dDwWZr5UA9bOJ0EG4j-magFdd4TrnbDgO-LhClrL2V-kxcre5Qo8hRValn6EoRYLq3NpdEktanqv_6o_WnlZyrTa3Yrj6I59pCjoF16";
     final String token2 = "e6e8Sxz2SL2lQyS1AF3XBd:APA91bF_CYulgRdobkmHuimqO0THEaRQnFA8Bp_IvCf4kslB5m_cQK73FLNHZpRvlsoCOpc9EB94pXsCtoWBH4l8IqJyKVTogIW3K0EdgSAvu8Omfhbe8mJvUHqtl2HfUoALn9X1q3s-";
-    final String token3 = "";
+    final String token3 = "eokZ6Dw2TPaUQyLqyPjsnU:APA91bG7eaPvQE7a-mBY6Plf6pF8u1XG-ihBGBfErdwE2o6TfbIjV0pcj3rTfEw6Pbxmq-gwjTsz0PZ-UbXudnj4wgHIK_MkV0tE7r1Ije-p9WbgebRn-lH_z8ZJ8C0_3P10v5s8mnBd";
     final String token4 = "";
 
     @Autowired
@@ -45,6 +45,7 @@ public class BreakdownController {
         logger.info("Dodano nową awarię na testerze: " + breakdown.getComputerName());
         firebaseMessagingService.sendNotificationByToken(new NotificationMessage(token1, breakdown.getComputerName(), breakdown.getFailureName()));
         firebaseMessagingService.sendNotificationByToken(new NotificationMessage(token2, breakdown.getComputerName(), breakdown.getFailureName()));
+        firebaseMessagingService.sendNotificationByToken(new NotificationMessage(token3, breakdown.getComputerName(), breakdown.getFailureName()));
         return new ResponseEntity<>(savedBreakdown, HttpStatus.CREATED);
     }
 
