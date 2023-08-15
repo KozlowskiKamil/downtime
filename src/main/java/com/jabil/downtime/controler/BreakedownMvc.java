aa3164d96deb63f0785acbc1bfd46649ba05f34c
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-15 17:36:50 +0200

    add list method

---
+++ src/main/java/com/jabil/downtime/controler/BreakedownMvc.java
@@ -0,0 +1,28 @@
+package com.jabil.downtime.controler;
+
+import com.jabil.downtime.BreakdownService;
+import com.jabil.downtime.dto.BreakdownDto;
+import com.jabil.downtime.model.Breakdown;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BreakedownMvc {
+
+
+    private final BreakdownService breakdownService;
+
+    @GetMapping("/list")
+    public String breakedownList(Model model) {
+        List<BreakdownDto> all = breakdownService.findAll();
+        model.addAttribute("breakdowns" , all);
+        return "list";
+    }
+
+}
