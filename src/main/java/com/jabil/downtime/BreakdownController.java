aceca34d266ff08dd8b981582c62f8babfa5199d
KozlowskiKamil <kamil2k8@gmail.com>
2023-08-22 11:23:57 +0200

    add list method

--- src/main/java/com/jabil/downtime/BreakdownController.java
+++ src/main/java/com/jabil/downtime/BreakdownController.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class BreakdownController {
 
     private static final Logger logger = LoggerFactory.getLogger(BreakdownController.class);
-    final String token1 = "e3e_TLX_TxSK3ii63nqLjW:APA91bFSaFIEMzBTpRmu7AN0uOrfDwOauYA53dDwWZr5UA9bOJ0EG4j-magFdd4TrnbDgO-LhClrL2V-kxcre5Qo8hRValn6EoRYLq3NpdEktanqv_6o_WnlZyrTa3Yrj6I59pCjoF16";
+    final String token1 = "cLGB1ZIoS2-AI6iRdIHKot:APA91bEYSjLbGV00OCDoFagVV6yougsFmSIf0TjkwYOBAGX_jP-5MvL-KHddTaKX0x6ezekp-96OCweH7GlAQfueMBjF7aoQEMvJJLOiSCml5Zl7Pohe_Dg6hYtnpJWcMCoV6090bx2q";
     final String token2 = "e6e8Sxz2SL2lQyS1AF3XBd:APA91bF_CYulgRdobkmHuimqO0THEaRQnFA8Bp_IvCf4kslB5m_cQK73FLNHZpRvlsoCOpc9EB94pXsCtoWBH4l8IqJyKVTogIW3K0EdgSAvu8Omfhbe8mJvUHqtl2HfUoALn9X1q3s-";
     final String token3 = "eokZ6Dw2TPaUQyLqyPjsnU:APA91bG7eaPvQE7a-mBY6Plf6pF8u1XG-ihBGBfErdwE2o6TfbIjV0pcj3rTfEw6Pbxmq-gwjTsz0PZ-UbXudnj4wgHIK_MkV0tE7r1Ije-p9WbgebRn-lH_z8ZJ8C0_3P10v5s8mnBd";
     final String token4 = "";
