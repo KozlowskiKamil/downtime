06af78927664897a2627b52dff7b7607cb8362ab
KozlowskiKamil <kamil2k8@gmail.com>
2023-09-14 21:55:05 +0200

    add todo class

---
+++ src/main/java/com/jabil/downtime/dto/TodoDto.java
@@ -0,0 +1,24 @@
+package com.jabil.downtime.dto;
+
+import com.jabil.downtime.model.Priority;
+import com.jabil.downtime.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDto {
+
+    Long id;
+    String task;
+    String client;
+    Status status;
+    LocalDateTime createTask;
+    Priority priority;
+}
---
+++ src/main/java/com/jabil/downtime/mapper/TodoMapper.java
@@ -0,0 +1,19 @@
+package com.jabil.downtime.mapper;
+
+import com.jabil.downtime.dto.TodoDto;
+import com.jabil.downtime.model.Todo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoMapper {
+
+//    public Todo fromDto(TodoDto todoDto) {
+//
+//    }
+//
+//
+//    public TodoDto todoDto(Todo todo) {
+//
+//
+//    }
+}
